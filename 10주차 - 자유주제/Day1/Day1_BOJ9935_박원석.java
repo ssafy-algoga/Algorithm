fbf611711f5613543ee69b7218176005d02aca44
parkwonseok <48585170+parkwonseok@users.noreply.github.com>
2021-03-30 22:34:19 +0900

    Day1 박원석 제출

--- 10주차 - 자유주제/Day1/Day1_BOJ9935_박원석.java
+++ 10주차 - 자유주제/Day1/Day1_BOJ9935_박원석.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Day1_BOJ9935_¹Ú¿ø¼® {
+public class Day1_BOJ9935_ë°•ì›ì„ {
 	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 	
 	public static void main(String[] args) throws IOException {
